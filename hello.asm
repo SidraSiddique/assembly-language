da098f2b7699544a1e23a11c5306200d32a94f83
Sidra Siddique <bsdsf22a030@pucit.edu.pk>
2025-01-19 00:47:22 -0700

    Create hello.asm

---
+++ hello.asm
@@ -0,0 +1,15 @@
+.model small
+.stack 100h
+.data
+	str1 db "hello world$"
+.code
+	main proc
+		mov ax,@data
+		mov ds,ax
+		mov dx,offset str1
+		mov ah,09h
+		int 21h
+	mov ah,4ch
+	int 21h
+main endp
+end main
